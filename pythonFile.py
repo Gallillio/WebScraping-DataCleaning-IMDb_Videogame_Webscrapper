0d566e60e52340f7d0b506e859506e6ca6b758d7
Ahmed Galal <117813417+Gallillio@users.noreply.github.com>
2023-03-29 19:55:44 +0200

    Ignore JupyterNotebook in Language Stat
    This is just a python file to replace JupyterNotebook with Python in the language stat to avoid confusion

---
+++ pythonFile.py
@@ -0,0 +1,2 @@
+#This is just a python file to replace JupyterNotebook with Python in the language stat to avoid confusion
+print("This is just a python file to replace JupyterNotebook with Python in the language stat to avoid confusion")
